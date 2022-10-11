9dfc058ee457d2ff11b03a8edd45d34e4de41bfc
Sumanbshollq <42889005+Sumanbshollq@users.noreply.github.com>
2022-10-11 21:23:22 +0530

    Add files via upload

---
+++ Suman_FOPJava_LabAlgo2/src/main/java/com/greatlearning/driver/Transaction.java
@@ -0,0 +1,11 @@
+package com.greatlearning.driver;
+
+import java.util.Scanner;
+
+public class Transaction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of the transcation Array : ");
+        int numberOfTransaction = scanner.nextInt();
+    }
+}
