7e2e7716fad353539199ac269c636467223e2a1e
Sumanbshollq <42889005+Sumanbshollq@users.noreply.github.com>
2022-10-13 09:19:21 +0530

    Update Transaction.java

--- Suman_FOPJava_LabAlgo2/src/main/java/com/greatlearning/driver/Transaction.java
+++ Suman_FOPJava_LabAlgo2/src/main/java/com/greatlearning/driver/Transaction.java
@@ -7,5 +7,31 @@ public class Transaction {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the size of the transcation Array : ");
         int numberOfTransaction = scanner.nextInt();
+        int transArray[] = new int[numberOfTransaction];
+        System.out.println("Enter the Transactions : ");
+        for (int i = 0; i < numberOfTransaction; i++) {
+            transArray[i] = scanner.nextInt();
+        }
+        System.out.println("What is the target needs to be achieved ?");
+        int target = scanner.nextInt();
+
+        //Logic
+        int checkVar = 0;
+        int idx = 1;
+        boolean checkCond = false;
+        for (int i = 0; i < numberOfTransaction; i++) {
+            checkVar += transArray[i];
+            idx = idx + i;
+            if (checkVar >= target) {
+                checkCond = true;
+                break;
+            }
+        }
+
+        if (checkCond) {
+            System.out.println("Target is achieved " + idx + " transactions");
+        } else {
+            System.out.println("Target is not achieved");
+        }
     }
 }
